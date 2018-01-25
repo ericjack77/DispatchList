20f9387b1bdd4acd4dc16487770b59fd02950e30
ericjack77 <ericjack7@gmail.com>
2018-01-26 00:34:45 +0800

    下個任務:編排check_work

---
+++ app/src/main/java/com/example/eric/dispatchlist/EmployeeActivity.java
@@ -0,0 +1,13 @@
+package com.example.eric.dispatchlist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EmployeeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employee);
+    }
+}
--- app/src/main/java/com/example/eric/dispatchlist/MainActivity.java
+++ app/src/main/java/com/example/eric/dispatchlist/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.eric.dispatchlist;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 import com.example.eric.dispatchlist.DAOdata.Crane;
@@ -19,26 +23,48 @@ import com.google.firebase.database.ValueEventListener;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Timer;
 
 public class MainActivity extends AppCompatActivity {
 
     public static DispatchDAO dao;
-
+    EditText etuser,etpass;
     FirebaseDatabase database;
     DatabaseReference myRef;
+    HashMap<String,String> mapuser,map,map2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        etuser =findViewById(R.id.etuser);
+        etpass = findViewById(R.id.etpass);
+        mapuser = new HashMap<>();
         dao =new DispatchDAO("Crane");
         dao =new DispatchDAO("employeeList");
         dao =new DispatchDAO("dispatchLists");
 
 
         database = FirebaseDatabase.getInstance(); //建立連結
+        loadFile(); //下載 自己的帳號密碼資料
+        if (mapuser.isEmpty() == false)
+        {
+            for(String key : mapuser.keySet()){
+                String value = mapuser.get(key);
+                Log.d("帳號=",key+",密碼="+value);
+                etuser.setText(key);
+                etpass.setText(value);
+            }
+
+
+        }
 
 
         dlCar();  //下載 車子資料
@@ -46,12 +72,59 @@ public class MainActivity extends AppCompatActivity {
         dlDispatch(); //下載 派工資料
 
 
+
     }
 
-    public void clickLogin(View v)
+
+
+    public void clickLogin(final View v)
     {
-        Intent it =new Intent(MainActivity.this,EmployerActivity.class);
-        startActivity(it);
+
+        if (dao.employeeList == null)
+        {
+             Toast.makeText(this, "系統讀取中,請稍後再按一次", Toast.LENGTH_LONG).show();
+        }
+        else {
+            map = new HashMap<>();  //抓帳號密碼資料庫
+            map2 = new HashMap<>();
+
+            for (Employee e:dao.employeeList)
+            {
+                map.put(e.username,e.password);
+                Log.d("帳號",e.username+",密碼"+e.password);
+                map2.put(e.username,e.position);
+            }
+
+            String user = etuser.getText().toString();
+            String pass=  etpass.getText().toString();
+            if (map.containsKey(user) && (map.get(user).equals(pass)) && (map2.get(user).equals("管理"))) //做分流
+            {
+                savaFile();
+                Intent it =new Intent(MainActivity.this,EmployerActivity.class);
+                startActivity(it);
+            }
+            else if (map.containsKey(user) && (map.get(user).equals(pass)) && (map2.get(user).equals("司機") || map2.get(user).equals("助手")))
+            {
+                savaFile();
+                Intent it =new Intent(MainActivity.this,EmployeeActivity.class);
+                startActivity(it);
+            }
+            else
+            {
+                AlertDialog.Builder builder =new AlertDialog.Builder(this);
+                builder.setTitle("錯誤");
+                builder.setMessage("帳號或密碼錯誤,請再重新輸入一次"+"\n"
+                        +"帳號為身份證字號首字為大寫");
+                builder.setPositiveButton("重新輸入", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                });
+                builder.show();
+            }
+
+        }
+
     }
 
     public void dlDispatch()
@@ -144,6 +217,42 @@ public class MainActivity extends AppCompatActivity {
                 Log.w("firebase", "Failed to read value.", error.toException());
             }
         });
+    }
+
+    public  void savaFile()
+    {
+        mapuser.clear();
+        mapuser.put(etuser.getText().toString(),etpass.getText().toString());
+        File f =new File(getFilesDir(),"user.txt");
+        FileWriter fw =null;
+        try {
+            fw = new FileWriter(f);
+            Gson gson = new Gson();
+            String data = gson.toJson(mapuser);
+            fw.write(data);
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void loadFile()
+    {
+        File f =new File(getFilesDir(),"user.txt");
+        FileReader fr = null;
+        try {
+            fr= new FileReader(f);
+            BufferedReader br = new BufferedReader(fr);
+            String str = br.readLine();
+            Gson gson = new Gson();
+            mapuser = gson.fromJson(str,new TypeToken<HashMap<String,String>>(){}.getType());
+            br.close();
+            fr.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
